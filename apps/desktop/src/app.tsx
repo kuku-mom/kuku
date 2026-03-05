f307d55e1311360f8c4b1b263250f795cdddece7
GyeongTaek Kim (mansuiki) <mansuiki@gmail.com>
2026-03-05 22:02:26 +0900

    feat: init tauri

--- apps/desktop/src/app.tsx
+++ apps/desktop/src/app.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>KUKU</div>;
+  return (
+    <>
+      <header
+        class="relative flex h-8.5 shrink-0 items-center justify-center select-none"
+        data-tauri-drag-region
+      ></header>
+      <div>KUKU</div>
+    </>
+  );
 }
